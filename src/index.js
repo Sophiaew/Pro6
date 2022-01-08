ccd480081d1cb9ebc5531c3cab2e1188b2c02a22
Sophiaew <Sophiaew@live.com>
2022-01-08 16:31:08 -0500

    switch units

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = `17bcfd67e085423bef87d025a6a15b1b`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(displayForecast);
 }
